cb1de406ed5fb67019d48b10662f7b3c926d3c10
linhn0617 <yyo833782@gmail.com>
2025-01-07 11:04:44 +0800

    docs: Add problem description and link for merge string alternately

--- 1768.php
+++ 1768.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * link:
+ * https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
+ * description:
+ * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+ * Return the merged string.
+ */
 class Solution
 {
     /**
@@ -50,4 +58,4 @@ class Solution {
         return $result;
     }
 }
-*/
+ */
