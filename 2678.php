2cc61eb538ac72228bd6e1f49c433e8ce935e7d4
linhn0617 <yyo833782@gmail.com>
2025-02-05 08:00:26 +0800

    feat: Implement countSeniors function to count people over 60 years old

---
+++ 2678.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * link:
+ * https://leetcode.com/problems/number-of-senior-citizens/description/
+ * description:
+ * You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+ * The first ten characters consist of the phone number of passengers.
+ * The next character denotes the gender of the person.
+ * The following two characters are used to indicate the age of the person.
+ * The last two characters determine the seat allotted to that person.
+ * Return the number of passengers who are strictly more than 60 years old.
+ */
+
+class Solution
+{
+
+    /**
+     * @param String[] $details
+     * @return Integer
+     */
+    function countSeniors($details)
+    {
+        // 定義超過60歲的人的計數器
+        $count = 0;
+        // 遍歷$details陣列
+        foreach ($details as $detail) {
+            // 判斷年齡是否超過60歲
+            if ((int)substr($detail, 11, 2) > 60) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
+}
