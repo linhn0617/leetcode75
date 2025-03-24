cc9580a4b0969b5f9f4830c0fe6d73df3408e86a
linhn0617 <yyo833782@gmail.com>
2025-03-24 22:28:19 +0800

    docs : add link and description

--- 217.php
+++ 217.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * link: https://leetcode.com/problems/contains-duplicate/
+ * description:
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+
 class Solution {
 
     /**
@@ -10,7 +16,7 @@ class Solution {
         $seen = []; // 建立一個空陣列來記錄已經看過的數字
     
         foreach ($nums as $num) { // 逐一檢查輸入陣列中的每個數字
-            if (isset($seen[$num])) { // 檢查這個數字是否已經在我們的 $seen 陣列中
+            if (isset($seen[$num])) { // 檢查這個數字是否已經在 $seen 陣列中
                 return true; // 如果已經存在，表示找到重複元素，立即返回 true
             }
             $seen[$num] = true; // 如果這個數字是第一次出現，將它標記為已看過
